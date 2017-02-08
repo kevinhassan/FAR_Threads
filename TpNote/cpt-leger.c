32d233a01f8a72d2886d47f06f7c3106eb5007bf
Kevin HASSAN <kevin.hassan@igtp3-mac12.isim.intra>
2017-02-08 08:50:42 +0100

    tp note #1

---
+++ TpNote/cpt-leger.c
@@ -0,0 +1,27 @@
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <unistd.h> 
+#include <pthread.h> 
+#define NB_THREADS 500 
+int somme=0;
+void * Fils(void *t) {
+	int i= (long)t;	
+	int tmp = somme;
+	tmp ++;
+	somme = tmp;
+	printf("Fils %d : somme = %d \n", i, somme); 
+	printf("Pid : %d \n", getpid());
+	pthread_exit(0);
+}
+
+int main() {
+	long i;
+	pthread_t thread[NB_THREADS]; 
+	for (i=0;i<NB_THREADS;i++)
+		pthread_create(&thread[i], NULL, Fils, (void *)i); 
+	for (i=0;i<NB_THREADS;i++) 
+		pthread_join(thread[i], NULL); 
+	printf("Pere : somme = %d \n", somme); 
+	//printf("Pid : %d \n", getpid());
+	return 0;
+}
---
+++ TpNote/cpt-lourd.c
@@ -0,0 +1,25 @@
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <unistd.h> 
+#include <pthread.h> 
+#define NB_PROCESSUS 500 
+
+int pid, i, somme ;
+int main()
+{ 
+	somme = 0 ;
+	for (i=0 ; i<NB_PROCESSUS ; i++) { 
+		pid=fork();
+		if (pid == 0) { 
+		  somme++ ;
+		  printf("processus fils %d : somme = %d \n", i, somme) ;
+		  printf("Pid : %d \n", getpid());
+		  exit(0) ; 
+		}
+	}
+
+	while ( wait(0) != -1) { } ;
+	printf("processus père : somme = %d \n", somme) ; 
+	printf("Pid : %d \n", getpid());
+	return 0 ;
+}
